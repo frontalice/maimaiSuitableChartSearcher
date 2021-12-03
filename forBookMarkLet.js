a2b90162c99e251feedf01e117733865fda383a2
frontalice <mayonakaTV@me.com>
2021-12-03 20:37:06 +0900

    add new file

---
+++ forBookMarkLet.js
@@ -0,0 +1,1 @@
+javascript:$(".user_body td[style*='width: 40px;']").each(function(c,a){var b=Number($(a).text());150<=b?$(a).css("background-color","red"):140>b&&$(a).css("background-color","aqua")});$(".user_body td[style*=%27width: 60px;%27]").each(function(c,a){var b=Number($(a).text());800<=b?$(a).css("background-color","red"):700>b&&16<=b&&$(a).css("background-color","aqua")});
