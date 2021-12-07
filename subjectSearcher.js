0927e4ad9291d5a9fe4a2ba12ad8e9fea291709f
frontalice <mayonakaTV@me.com>
2021-12-08 01:35:18 +0900

    wip ver. added

--- subjectSearcher.js
+++ subjectSearcher.js
@@ -1,3 +1,4 @@
+// 現行バージョン
 javascript:
 $(".user_body td[style*='width: 40px;']").each(function(index, elem) {
 	const bpm = Number($(elem).text())
@@ -19,3 +20,33 @@ $(".user_body td[style*='width: 60px;']").each(function(index, elem) {
 		$(elem).css('background-color', 'lime');
 	}
 })
+
+// 開発中
+javascript:
+	$(".user_body table:first").remove();
+	$(".user_body > ul").remove();
+	$(".user_body table").each(function(index, elem) {
+			for (let i=$(elem).find("tr").length-1; i>1; i--) {
+				const $bpm = $(elem).find("tr:eq(i)").find("td:eq(2)");
+				const BPM = Number($($bpm).text());
+				const $notes = $(elem).find("tr:eq(i)").find("td:eq(3)");
+				const NOTES = Number($($notes).text());
+				if (BPM >= 140 && BPM < 150) {
+					$($bpm).css("background-color", "lime");
+				} else if (BPM >= 130 && BPM < 140) {
+					$($bpm).css("background-color", "aqua");
+				} else if (BPM >= 150 && BPM < 160) {
+					$($bpm).css("background-color", "fuchsia");
+				}
+				if (NOTES >= 700 && NOTES < 800) {
+					$($notes).css("background-color", "lime");
+				} else if (NOTES >= 16 && NOTES < 700) {
+					$($notes).css("background-color", "aqua");
+				} else if (NOTES >= 800 && NOTES < 900) {
+					$($notes).css("background-color", "fuchsia");
+				}
+				/* if ($($bpm).css("background-color") == "#FFFFFF" && $($notes).css("background-color") == "#FFFFFF"){
+					$(elem).find("tr:eq(i)").remove();
+				} */
+			}
+	})
